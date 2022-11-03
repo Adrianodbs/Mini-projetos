8418b11cd5fb5ef231bc93dcf47d440e9696c95f
Adriano <adriano_alves_2008@hotmail.com>
2022-11-03 10:14:15 -0300

    iniciando a class (calculadora)

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -3,13 +3,24 @@ const currentOperationText = document.querySelector('#current-operation')
 
 const buttons = document.querySelectorAll('#buttons-container button')
 
-class Calculator {}
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText
+    this.currentOperationText = currentOperationText
+    this.currentOperation = ''
+  }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach(btn => {
   btn.addEventListener('click', e => {
     //Para pegar o valor dos botões
     const value = e.target.innerText
 
-    console.log(value)
+    //Para observar se está clicando em um número
+    if (+value >= 0 || value === '.') {
+    } else {
+    }
   })
 })
